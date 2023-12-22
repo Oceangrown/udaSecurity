5c3c9450815138cfcd26e8b8ec5b8cc15f8b3c49
Oceangrown <yankeejake91@gmail.com>
2023-12-22 12:12:57 -0800

    check

---
+++ udaSecurity/app/src/main/java/org/udaSecurity/app/CatpointApp.java
@@ -0,0 +1,14 @@
+package org.udaSecurity.app;
+
+
+import org.udaSecurity.app.Gui.CatpointGui;
+
+/**
+ * This is the main class that launches the application.
+ */
+public class CatpointApp {
+    public static void main(String[] args) {
+        CatpointGui gui = new CatpointGui();
+        gui.setVisible(true);
+    }
+}
---
+++ udaSecurity/security/src/main/java/org/udaSecurity/security/StatusListener.java
@@ -0,0 +1,12 @@
+package org.udaSecurity.security;
+
+import org.udaSecurity.security.data.AlarmStatus;
+
+/**
+ * Identifies a component that should be notified whenever the system status changes
+ */
+public interface StatusListener {
+    void notify(AlarmStatus status);
+    void catDetected(boolean catDetected);
+    void sensorStatusChanged();
+}
